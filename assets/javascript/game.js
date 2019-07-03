d5758a6f78dab228da525365e51ffe8087f5fef2
Bryan Villalon <bavillalon@gmail.com>
2019-07-02 23:34:38 -0500

    further improvements to logic. added reset. started making adjustments to the style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,56 +9,78 @@ var goodGuess=false;
 var usedLetter=false;
 var strikes=0;
 var gameStatus="";
+var newWord=true;
+var wins=0;
+var losses=0;
 
 
 
-wordToGuess=words[Math.floor(Math.random()*words.length)];
-console.log(wordToGuess);
-for(var i=0;i<wordToGuess.length; i++){
-    if (wordToGuess[i]===" "){
-        printToScreen+=" ";
-    }
-    else{
-    printToScreen+="_";
-    }
-}
 
 document.onkeyup=function(event){
     usedLetter=false;
     currentLetterGuess = event.key.toLowerCase();
+
+    if(newWord){
+        wordToGuess=words[Math.floor(Math.random()*words.length)];
+        console.log(wordToGuess);
+        for(var i=0;i<wordToGuess.length; i++){
+            if (wordToGuess[i]===" "){
+                printToScreen+=" ";
+            }
+            else{
+            printToScreen+="_";
+            }
+        }
+        newWord=false;
+    }
     
     for(var i=0; i<userGuesses.length;i++){
         usedLetter=usedLetter||userGuesses[i]===currentLetterGuess;
     }
 
-    if(strikes!=6&&printToScreen!=wordToGuess){
-        if(!usedLetter){
-            userGuesses=userGuesses.concat(currentLetterGuess);
-            goodGuess=false;
-            for(var i=0; i<wordToGuess.length; i++){
-                if(currentLetterGuess===wordToGuess[i].toLowerCase()){
-                    printToScreen=printToScreen.substring(0,i)+wordToGuess[i]+printToScreen.substring(i+1);
-                    goodGuess=true;
-                    gameStatus="Good guess!"
+
+    if(currentLetterGuess.length===1&&(currentLetterGuess!=" ")){
+        if( (strikes!=6) && (printToScreen!=wordToGuess) ){
+            if(!usedLetter){
+                userGuesses=userGuesses.concat(currentLetterGuess);
+                goodGuess=false;
+                for(var i=0; i<wordToGuess.length; i++){
+                    if(currentLetterGuess===wordToGuess[i].toLowerCase()){
+                        printToScreen=printToScreen.substring(0,i)+wordToGuess[i]+printToScreen.substring(i+1);
+                        goodGuess=true;
+                        gameStatus="Good guess!"
+                    }
+                }
+
+                if(!goodGuess){
+                    incorrectGuesses=incorrectGuesses.concat(currentLetterGuess);
+                    strikes++;
+                    gameStatus="Ooooo bad luck there..."
                 }
             }
-
-            if(!goodGuess){
-                incorrectGuesses=incorrectGuesses.concat(currentLetterGuess);
-                strikes++;
-                gameStatus="Ooooo bad luck there..."
+            else{
+                gameStatus="You've already guessed that letter!"
             }
-        }
-        else{
-            gameStatus="You've already guessed that letter!"
         }
     }
 
      if(strikes===6){
-        gameStatus="You are a loser."
+        gameStatus="You lost the last game. A new game just started"
+        currentLetterGuess="";
+        newWord=true;
+        printToScreen="";
+        incorrectGuesses="";
+        strikes=0;
+        userGuesses="";
     }
     else if(printToScreen===wordToGuess){
-        gameStatus="This is a win win win siituation!"
+        gameStatus="This is a win win win situation! Play again to see if your luck continues"
+        currentLetterGuess="";
+        newWord=true;
+        printToScreen="";
+        incorrectGuesses="";
+        strikes=0;
+        userGuesses="";
     }
 
     
